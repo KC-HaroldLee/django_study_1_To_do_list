96a9498ddd6aeb992232c1e64bc4a6f151fff0a4
KC-Harold <kklastchnpd@naver.com>
2022-04-11 23:05:39 +0900

    호스트 승인

--- toDoList/toDoList/settings.py
+++ toDoList/toDoList/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_vsw9pb5(0pjngpxf5p3e5sed))))sg3_s0i$n76y(u%=xc04e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
